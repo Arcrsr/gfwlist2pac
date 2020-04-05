438afe4917ddfd94e5b2a7ee1e897f6f837adfcc
吕辰 <672725321@qq.com>
2020-04-05 15:34:24 +0800

    [Sun Apr  5 15:34:24     2020]auto update

--- calil.sh
+++ calil.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
-git reset --hard
-git pull
+# git reset --hard
+# git pull
 
 git submodule update --init
 for i in gfwlist genpac
@@ -9,18 +9,14 @@ do
 	(cd $i;git pull origin master)
 done
 
-rm -rf env
-virtualenv env
-source env/bin/activate
 (cd genpac;python setup.py install)
 
-env/bin/genpac \
-	--pac-proxy "SOCKS5 127.0.0.1:1080" \
+genpac \
+	--pac-proxy "SOCKS5 127.0.0.1:1081; SOCKS 127.0.0.1:1081; DIRECT;" \
 	--gfwlist-url - \
 	--gfwlist-local gfwlist/gfwlist.txt \
 	-o gfwlist.pac
 sed -e '5d' -e '3d' -i gfwlist.pac
-deactivate
 
 git add .
 git commit -m "[$(LANG=C date)]auto update"
